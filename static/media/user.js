8312c1d5251a90d9de74a6f91085c1792921fe02
Sagor <mbrsagor@gmail.com>
2021-01-07 12:44:48 +0600

    List of users json file created

---
+++ static/media/user.js
@@ -0,0 +1,5 @@
+const users = [
+    { name: 'Mbr-sagor', email:'mbrsagor@gmail.com', password:'demo', phone:'01748880505', address:'Dhaka, Bangladesh'},
+    { name: 'Md.Ohi', email:'ohi@gmail.com', password:'ohi', phone:'01700000000', address:'Dhaka, Bangladesh'},
+    { name: 'Md.meg', email:'meg@gmail.com', password:'meg', phone:'01800000000', address:'Dhaka, Bangladesh'},
+]
